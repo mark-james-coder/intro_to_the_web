662d61abc66d99e169a48fac8bfaccbbf23958c5
Mark <markjpoole@yahoo.com>
2020-07-21 15:55:35 +0100

    use params to extract information from a request

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 set :session_secret, 'the secret'
 
-get '/cat' do
+get '/random-cat' do
   @name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params[:name]
+  @name = params[:name]
   erb(:index)
 end
